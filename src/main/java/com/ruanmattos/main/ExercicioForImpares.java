e97a409210f6196b8175d5fc0b520e6559bbb818
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-12 19:14:02 -0300

    Implementações feitas e testadas

--- src/main/java/com/ruanmattos/main/ExercicioForImpares.java
+++ src/main/java/com/ruanmattos/main/ExercicioForImpares.java
@@ -1,16 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.ruanmattos.main;
 
+import java.util.Scanner;
+
 /**
+ * 1. Leia um valor inteiro X. Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
  *
- * @author aluno
+ * ENTRADA: Número digitado 8
+ *
+ * SAIDA: Tela do programa 1 3 5 7
+ *
+ * @author Ruan Pereira Mattos
  */
 public class ExercicioForImpares {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int num = s.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if ((i % 2) != 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
